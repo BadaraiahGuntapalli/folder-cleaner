f16aa3b74c8afa98e4fbbddac1da7f4cbf11975a
BDX <gbadaraiah@gmail.com>
2026-03-13 17:32:27 +0530

    Restrucuture as proper Python pacakge v0.1.0

--- clean.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-:: %~dp0 = the folder this .bat file lives in (dynamic, works for any user)
-:: %USERPROFILE% = current user's home directory (e.g. C:\Users\YourName)
-python "%~dp0cleaner.py" --folder "%USERPROFILE%\Downloads" %*
